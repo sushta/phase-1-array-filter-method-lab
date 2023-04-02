b8794b81f5d88864b30a3383df13ed869848e2a6
sustapakhreen <sushta48@gmail.com>
2023-04-02 12:57:08 -0500

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(arrs, query) {
+    return arrs.filter(item => item.toLowerCase().includes(query.toLowerCase()));
+  }
+
+function fuzzyMatch(arrs, query) {
+    return arrs.filter(item => item.toLowerCase().startsWith(query.toLowerCase()));
+  }
+
+function matchName(arrs, query) {
+    return arrs.filter(driver => driver.name === query);
+  }
